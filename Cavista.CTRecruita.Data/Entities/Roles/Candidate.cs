5953ff26ae8c54742aa44d21187a30f6a4bcb306
tanosike <tanosike@AXNIGCELAP4651>
2026-09-08 13:04:45 +0100

    chore: nullify linkedin url

--- Cavista.CTRecruita.Data/Entities/Roles/Candidate.cs
+++ Cavista.CTRecruita.Data/Entities/Roles/Candidate.cs
@@ -7,9 +7,9 @@ namespace Cavista.CTRecruita.Data.Entities.Roles
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Source { get; set; }
-        public string LinkedInUrl { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Source { get; set; }
+        public string? LinkedInUrl { get; set; }
         public ICollection<Application> Applications { get; set; } = new List<Application>();
     }
 }
---
+++ Cavista.CTRecruita.Web/Migrations/20260908120416_MakeCandidatesLinkedInUrlNullable.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cavista.CTRecruita.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCandidatesLinkedInUrlNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "Candidates",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Candidates",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LinkedInUrl",
+                table: "Candidates",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Candidates",
+                keyColumn: "Source",
+                keyValue: null,
+                column: "Source",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "Candidates",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Candidates",
+                keyColumn: "PhoneNumber",
+                keyValue: null,
+                column: "PhoneNumber",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Candidates",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Candidates",
+                keyColumn: "LinkedInUrl",
+                keyValue: null,
+                column: "LinkedInUrl",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LinkedInUrl",
+                table: "Candidates",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
